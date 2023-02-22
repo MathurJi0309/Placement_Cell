bf1b4e263325b7ab2ac5e03153db73ed3bdceae4
MathurJi <anileshjjssacademy84@gmail.com>
2023-02-23 00:11:56 +0530

    make the basic setUp for the project

---
+++ controllers/user_controller.js
@@ -0,0 +1,9 @@
+module.exports.profile=function(req,res){
+    return res.render('user_profile',{
+        title:'User Profile'
+    })
+}
+
+// module.exports.signIn=function(req,res){
+
+// }
---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+// const bodyParser = require("body-parser");
+
+const app=express();
+
+app.use(express.static('./assets'));
+
+// app.use(bodyParser.urlencoded({extended: false,}));
+
+app.set('view engine','ejs');
+app.set('views','./views')
+
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(8000,function(){
+    console.log("app is running smotthly on the port 8000");
+})
